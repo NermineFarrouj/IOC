c0430e323603a60851363e6aee92882984e61a0c
NermineFarrouj <farroujnermine@gmail.com>
2025-04-16 18:57:07 +0100

    Sauvegarde temporaire avant changement de branche

---
+++ src/main/java/dao/DaoImpl.java
@@ -0,0 +1,11 @@
+package dao;
+
+public class DaoImpl implements IDao {
+
+    @Override
+    public double getData() {
+        System.out.println("Version base do données");
+        double temp=23;
+        return temp;
+    }
+}
